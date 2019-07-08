edee5165e6bf9f9f9d11cb4177cfc6314739ace7
David Hopkins <david.glenn.hopkins@gmail.com>
2019-07-07 20:59:52 -0700

    testing commits on android studio

--- app/src/main/java/com/hopkinsdynamics/testcalc/MainActivity.java
+++ app/src/main/java/com/hopkinsdynamics/testcalc/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // look something kinda random
         int myNum=0;
+        String nyName = "David";
     }
 }
