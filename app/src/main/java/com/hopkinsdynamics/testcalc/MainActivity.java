a167ce1110dcbcedc2062104824e631bc64bd9b2
David Hopkins <david.glenn.hopkins@gmail.com>
2019-07-21 15:50:09 -0700

    mostly functional, in process of adding Negative Button

--- app/src/main/java/com/hopkinsdynamics/testcalc/MainActivity.java
+++ app/src/main/java/com/hopkinsdynamics/testcalc/MainActivity.java
@@ -2,7 +2,7 @@ package com.hopkinsdynamics.testcalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,14 +13,23 @@ public class MainActivity extends AppCompatActivity {
     private EditText newNumber;
     private TextView displayOperation;
 
+    private static final String TAG = "MainActivity";
+    private final String TEXT_CONTENTS = "TextContents";
+
+
     // Variables to hold the operands and the type of calculations
     private Double operand1 = null;
-    private Double operand2 = null;
+  //  private Double operand2 = null;
     private String pendingOperation = "=";
+    private final String OPERATION_CONTENTS = "OpContents";
+    private final String OPERAND_CONTENTS="OperandContents";
 
+    private static final String STATE_PENDING_OPERATION="PendingOperation";
+    private static final String STATE_OPERAND1 = "Operand1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: in");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -64,14 +73,138 @@ public class MainActivity extends AppCompatActivity {
         button8.setOnClickListener(listener);
         button9.setOnClickListener(listener);
         buttonDot.setOnClickListener(listener);
-        buttonEquals.setOnClickListener(listener);
-        buttonDivide.setOnClickListener(listener);
-        buttonMultiply.setOnClickListener(listener);
-        buttonMinus.setOnClickListener(listener);
-        buttonPlus.setOnClickListener(listener);
 
+        View.OnClickListener opListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button b = (Button) view;
+                String op = b.getText().toString();
+                String value = newNumber.getText().toString();
+//                if(value.length() != 0){
+//                    performOperation(value, op);
+//                }
+                try{
+                    Double doubleValue = Double.valueOf(value);
+                    performOperation(doubleValue, op);
+                }catch(NumberFormatException e){
+                    newNumber.setText("");
+                }
+                pendingOperation = op;
+                displayOperation.setText(pendingOperation);
+            }
+        };
 
+        buttonEquals.setOnClickListener(opListener);
+        buttonDivide.setOnClickListener(opListener);
+        buttonMultiply.setOnClickListener(opListener);
+        buttonMinus.setOnClickListener(opListener);
+        buttonPlus.setOnClickListener(opListener);
+        Log.d(TAG, "onCreate: out");
+    }
 
+    private void performOperation(Double value, String operation){
+       // displayOperation.setText(operation);
+        if ( null == operand1){
+            operand1 = value;//Double.valueOf(value);
+        }else {
+           // operand2 = value;//Double.valueOf(value);
+            if( pendingOperation.equals("=")){
+                pendingOperation = operation;
+            }
+            switch(pendingOperation){
+                case "=": operand1 = value;// operand2;
+                    break;
+                case "/":
+                    if(/*operand2*/ value == 0){
+                        operand1=0.0;
+                    }else {
+                        operand1/= value;//operand2;
+                    }
+                    break;
+                case "*":
+                    operand1*= value;
+                    break;
+                case "-":
+                    operand1 -= value;//operand2;
+                    break;
+                case "+":
+                    operand1+=value;//operand2;
+                    break;
+            }
+        }
+
+        result.setText(operand1.toString());
+        newNumber.setText("");
 
     }
+//
+//    @Override
+//    protected void onStart() {
+//        Log.d(TAG, "onStart: in");
+//        super.onStart();
+//        Log.d(TAG, "onStart: out");
+//
+//    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: in");
+
+        super.onRestoreInstanceState(savedInstanceState);
+
+        pendingOperation =savedInstanceState.getString(STATE_PENDING_OPERATION);
+        displayOperation.setText(pendingOperation);
+        operand1=savedInstanceState.getDouble(STATE_OPERAND1);
+
+        Log.d(TAG, "onRestoreInstanceState: out");
+    }
+
+//    @Override
+//    protected void onResume() {
+//        Log.d(TAG, "onResume: in");
+//        super.onResume();
+//        Log.d(TAG, "onResume: out");
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        Log.d(TAG, "onPause: in");
+//        super.onPause();
+//        Log.d(TAG, "onPause: out");
+//    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+       // Log.d(TAG, "onSaveInstanceState: in");
+        outState.putString(STATE_PENDING_OPERATION, pendingOperation);
+        if(operand1 != null){
+            outState.putDouble(STATE_OPERAND1,operand1);
+        }
+
+        super.onSaveInstanceState(outState);
+      //  Log.d(TAG, "onSaveInstanceState: out");
+    }
+
+//    @Override
+//    protected void onStop() {
+//        Log.d(TAG, "onStop: in");
+//        super.onStop();
+//        Log.d(TAG, "onStop: out");
+//    }
+//
+//
+//    @Override
+//    protected void onRestart() {
+//        Log.d(TAG, "onRestart: in");
+//        super.onRestart();
+//        Log.d(TAG, "onRestart: out");
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        Log.d(TAG, "onDestroy: in");
+//        super.onDestroy();
+//        Log.d(TAG, "onDestroy: out");
+//    }
+
 }
